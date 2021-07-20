bb30cca602ff914f6dfab8fa0eb26261030cd915
Helia <jyk.helia@gmail.com>
2021-07-20 16:45:32 +0900

    [ Feature ] 로그인 틀

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-// import Login from './pages/login';
-// import SignUp from './pages/signup';
-import React from 'react';
+import Login from './pages/login';
+import SignUp from './pages/signup';
+import React, { useState } from 'react';
 
 function App() {
-  // const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false)
   // console.log(isLoggedIn)
 
   return (
     <div>
-      {/* <Login setIsLoggedIn={setIsLoggedIn} />
+      <Login setIsLoggedIn={setIsLoggedIn} />
       {isLoggedIn && <div>로그인 성공(&&)!</div>}
       {isLoggedIn ? <div>로그인 성공(삼항)!</div> : <div>로그인 실패!</div>}
-      <SignUp /> */}
+      <SignUp />
     </div>
   );
 }
