9faebbc991aa2228e1e814fb43738f78bce4c730
Helia <jyk.helia@gmail.com>
2021-07-21 20:00:39 +0900

    [ Update ] Signup form및 화면상호작용

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 
 function SignUp(props) {
   const [userInfo, setUserInfo] = useState({
@@ -7,26 +7,51 @@ function SignUp(props) {
     password: '',
     email: '',
     address: {
-      city: '',
+      city: '시티',
       gu: '',
       dong: ''
     },
     // 번호만 입력받기
     phoneNumber: '',
     //HowLongLiveAlone
-    livingAloneNthYears: ''
+    livingAloneYear: ''
   });
 
-  const { userId, nickname, password, email, address, phoneNumber, livingAloneNthYears } = userInfo;
+  // 구조분해 할당
+  let { userId, nickname, password, email, address, phoneNumber, livingAloneYear } = userInfo;
+
+  // 몇년차인지 표시하기 
+  const date = new Date();
+
+  // DOM 선택
+  const radioBtn = useRef();
+  const selectInputs = useRef();
 
   const onChange=useCallback(
     e => {
+
+      // select를 누르면 radio가 초기화
+      if (e.target.type === 'select-one') {
+        radioBtn.current.checked = false;
+      };
+
       const { name, value } = e.target;
       console.log(name, value);
-      setUserInfo({
-        ...userInfo,
-        [name]: value
-      });
+      // 주소만 구조가 다르므로 name이 주소 속성이면
+      if (Object.keys(address).includes(name)) {
+        setUserInfo({
+          ...userInfo,
+          address: {
+            ...userInfo.address,
+            [name]: value
+          }
+        });
+      } else {
+        setUserInfo({
+          ...userInfo,
+          [name]: value
+        });
+      }
     },
     [userInfo]
   );
@@ -77,20 +102,21 @@ function SignUp(props) {
         <option value="인천">인천</option>
         <option value="부산">부산</option>
       </select>
-      <label htmlFor="cars">Choose a car:</label>
-      <select id="cars" name="cars" value={address.gu} onChange={onChange}>
+      <label htmlFor="gu">Choose a gu:</label>
+      <select id="gu" name="gu" value={address.gu} onChange={onChange}>
         <option value="volvo">Volvo</option>
         <option value="saab">Saab</option>
         <option value="fiat">Fiat</option>
         <option value="audi">Audi</option>
       </select>
-      <label htmlFor="cars">Choose a car:</label>
-      <select id="cars" name="cars" value={address.dong} onChange={onChange}>
+      <label htmlFor="dong">Choose a dong:</label>
+      <select id="dong" name="dong" value={address.dong} onChange={onChange}>
         <option value="volvo">Volvo</option>
         <option value="saab">Saab</option>
         <option value="fiat">Fiat</option>
         <option value="audi">Audi</option>
       </select>
+
       <p>휴대전화 번호를 입력하세요</p>
       <input
         type="text"
@@ -99,12 +125,30 @@ function SignUp(props) {
         onChange={onChange}
       />
       <p>자취 몇년차이신가요?</p>
+      <p>언제 자취를 시작하셨나요?</p>
+      <label htmlFor="livingAloneYear">When did you start living alone</label>
       <input
-        type="number"
-        name="livingAloneNthYears"
-        value={livingAloneNthYears}
+        type='radio' 
+        name='livingAloneYear' 
+        value="null" 
         onChange={onChange}
-      />
+        ref={radioBtn}
+      /> 자취하지 않음 / 준비중
+      <select id="livingAloneYear" name="livingAloneYear" value={livingAloneYear} onChange={onChange} ref={selectInputs}>
+        <option defaultValue value="undefined"> -- 연도를 선택해주세요 -- </option>
+        <option value="2021">2021년</option>
+        <option value="2020">2020년</option>
+        <option value="2019">2019년</option>
+        <option value="2018">2018년</option>
+        <option value="2017">2017년</option>
+        <option value="2016">2016년</option>
+        <option value="2015">2015년</option>
+        <option value="2014">2014년</option>
+        <option value="2013">2013년</option>
+        <option value="2012">2012년 이전</option>
+      </select>
+      <p>자취{date.getFullYear()-livingAloneYear+1}년차!</p>
+
       <button type="submit">
         Signup
       </button>
