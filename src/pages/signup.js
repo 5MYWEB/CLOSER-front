90c543fc4b7bf7368541710b183d17123013d580
tjatn304905 <tjatn304905@gmail.com>
2021-07-22 15:37:14 +0900

    signup 페이지 axios 및 axios parameter 수정

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,4 +1,5 @@
 import React, { useState, useCallback, useRef } from 'react';
+import axios from 'axios'
 
 function SignUp(props) {
   const [userInfo, setUserInfo] = useState({
@@ -6,22 +7,25 @@ function SignUp(props) {
     nickname: '',
     password: '',
     email: '',
-    address: {
-      city: '시티',
+    addr: {
+      city: '서울',
       gu: '',
       dong: ''
     },
+    // addr: '',
     // 번호만 입력받기
     phoneNumber: '',
     //HowLongLiveAlone
-    livingAloneYear: ''
+    homeAlone: ''
   });
 
   // 구조분해 할당
-  let { userId, nickname, password, email, address, phoneNumber, livingAloneYear } = userInfo;
+  let { userId, nickname, password, email, addr, phoneNumber, homeAlone } = userInfo;
 
   // 몇년차인지 표시하기 
   const date = new Date();
+
+  // 주소 전체응
 
   // DOM 선택
   const radioBtn = useRef();
@@ -37,12 +41,13 @@ function SignUp(props) {
 
       const { name, value } = e.target;
       console.log(name, value);
+
       // 주소만 구조가 다르므로 name이 주소 속성이면
-      if (Object.keys(address).includes(name)) {
+      if (Object.keys(addr).includes(name)) {
         setUserInfo({
           ...userInfo,
-          address: {
-            ...userInfo.address,
+          addr: {
+            ...userInfo.addr,
             [name]: value
           }
         });
@@ -55,12 +60,35 @@ function SignUp(props) {
     },
     [userInfo]
   );
+  
+  // 회원가입 함수
+  const signup = (signupInfo) => {
+    const request = axios.post('http://localhost:8080/user/signup', signupInfo )
+      .then((response) =>{
+        console.log(response);
+        console.log(request);
+      })
+    return null
+  };
 
+  // 제출버튼 클릭 시 실행되는 함수
   const onSubmit=(
     e => {
       e.preventDefault();
-      console.log(userInfo);
-      props.setIsSignedUp(true);
+      // 백으로 보내질 주소
+      userInfo.addr = String(userInfo.addr.city + userInfo.addr.gu + userInfo.addr.dong);
+
+      console.log(userInfo)
+      // 그외 정보들이 입력되었는지 검사
+
+      // 회원가입 함수의 파라미터 설정
+      const signupInfo = {}
+      // console.log(signupInfo);
+
+      // 회원가입 함수 실행
+      signup(signupInfo);
+
+      // props.setIsSignedUp(true);
     }
   )
 
@@ -96,25 +124,25 @@ function SignUp(props) {
       />
       <p>주소를 입력하세요</p>
       <label htmlFor="city">Choose a City:</label>
-      <select id="city" name="city" value={address.city} onChange={onChange}>
-        <option value="서울">서울</option>
-        <option value="경기">경기</option>
-        <option value="인천">인천</option>
-        <option value="부산">부산</option>
+      <select id="city" name="city" value={addr.city} onChange={onChange}>
+        <option value="서울시">서울시</option>
+        <option value="용인시">용인시</option>
+        <option value="인천시">인천시</option>
+        <option value="부산시">부산시</option>
       </select>
       <label htmlFor="gu">Choose a gu:</label>
-      <select id="gu" name="gu" value={address.gu} onChange={onChange}>
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="fiat">Fiat</option>
-        <option value="audi">Audi</option>
+      <select id="gu" name="gu" value={addr.gu} onChange={onChange}>
+        <option value="동작구">동작구</option>
+        <option value="마포구">마포구</option>
+        <option value="서대문구">서대문구</option>
+        <option value="동구">동구</option>
       </select>
       <label htmlFor="dong">Choose a dong:</label>
-      <select id="dong" name="dong" value={address.dong} onChange={onChange}>
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="fiat">Fiat</option>
-        <option value="audi">Audi</option>
+      <select id="dong" name="dong" value={addr.dong} onChange={onChange}>
+        <option value="염리동">염리동</option>
+        <option value="동교동">동교동</option>
+        <option value="서교동">서교동</option>
+        <option value="둔산동">둔산동</option>
       </select>
 
       <p>휴대전화 번호를 입력하세요</p>
@@ -126,15 +154,15 @@ function SignUp(props) {
       />
       <p>자취 몇년차이신가요?</p>
       <p>언제 자취를 시작하셨나요?</p>
-      <label htmlFor="livingAloneYear">When did you start living alone</label>
+      <label htmlFor="homeAlone">When did you start living alone</label>
       <input
         type='radio' 
-        name='livingAloneYear' 
+        name='homeAlone' 
         value="null" 
         onChange={onChange}
         ref={radioBtn}
       /> 자취하지 않음 / 준비중
-      <select id="livingAloneYear" name="livingAloneYear" value={livingAloneYear} onChange={onChange} ref={selectInputs}>
+      <select id="homeAlone" name="homeAlone" value={homeAlone} onChange={onChange} ref={selectInputs}>
         <option defaultValue value="undefined"> -- 연도를 선택해주세요 -- </option>
         <option value="2021">2021년</option>
         <option value="2020">2020년</option>
@@ -147,7 +175,7 @@ function SignUp(props) {
         <option value="2013">2013년</option>
         <option value="2012">2012년 이전</option>
       </select>
-      <p>자취{date.getFullYear()-livingAloneYear+1}년차!</p>
+      <p>자취{date.getFullYear()-homeAlone+1}년차!</p>
 
       <button type="submit">
         Signup
